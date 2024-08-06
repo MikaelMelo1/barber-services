2f142f6a26ce33c89cbba4561e43820e4245b3b5
Mikael Melo <mikaelsilva1940@hotmail.com>
2024-08-06 11:50:52 -0300

    chore: initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    tsconfigRootDir: __dirname,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint/eslint-plugin'],
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  root: true,
+  env: {
+    node: true,
+    jest: true,
+  },
+  ignorePatterns: ['.eslintrc.js'],
+  rules: {
+    'prettier/prettier': ['error', {
+      semi: false, //ponto e virgula
+      singleQuote: true, //aspas simples
+      arrowParens: "void", //parenteses em funções de flecha
+      useTabs: true, //usar tabs
+      trailingComma:"nome" //virgula no final
+    }, { usePrettierrc: true }],
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+  },
+};
